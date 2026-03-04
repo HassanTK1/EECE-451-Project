f645162c2c3054fedd9b63c2987d9ef601cad0c4
Hassan Takruri <hassantakruri@gmail.com>
2026-03-04 19:32:52 +0200

    schema
    Define Pydantic models for health, identification, measurements, and statistics responses.

---
+++ backend/schema.py
@@ -0,0 +1,55 @@
+from pydantic import BaseModel, Field
+from typing import Optional, List, Literal
+from datetime import datetime
+
+
+class Health_response(BaseModel):
+    status: Literal["ok"] = "ok"
+    time : datetime
+
+
+class Identification_request(BaseModel):
+
+    device_id: str 
+    mac_address: Optional[str] = None
+
+
+class Identification_response(BaseModel):
+    device_id: str
+    first_meet: bool
+    first_seen: datetime
+    last_seen: datetime
+
+
+class Measurements_request(BaseModel):
+    device_id: str
+    operator: str
+    signal_power: int # in decibels
+    SNR : float[Optional] = None
+    network_type: Literal["2G","3G","4G"]   ### FOCUS - type might be better categorized
+    frequency_band: int # review data format
+    cell_id: str # maybe str 
+    time_stamp: datetime    
+
+
+class Measurements_response(BaseModel):
+     status: Literal["ok"] = "ok"
+     measurement_num: int
+     server_time: datetime
+
+
+
+class Stats_response(BaseModel):
+    from_date: datetime
+    to_date: datetime
+    avg_connectivity_operator: List
+    avg_connectivity_network: List
+    avg_signal_power_networkType: List
+    avg_signal_power_device: List
+    avg_SNR_SNIR: List
+    
+
+
+
+
+
