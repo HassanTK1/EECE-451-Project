8d22313a3e7ff37ea55a63b053d36049090be061
GarodBederian <garodbedirian@gmail.com>
2026-03-04 19:39:34 +0200

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from datetime import datetime
+from schema import *
 
 app = FastAPI()
 
@@ -6,7 +8,52 @@ app = FastAPI()
 def root():
     return {"message":"server is running"}
 
-
 @app.get("/health")
 def health():
     return { "status":"ok"}
+
+
+devices = {}
+@app.post("/identification")
+def identify(body: Identification_request):
+    if body.device_id in devices:
+        first_seen = devices[body.device_id]["first_seen"]
+        last_seen=datetime.now()
+        devices[body.device_id]["last_seen"] = last_seen
+        first_meet=False
+        
+        
+    else:
+        first_meet=True
+        first_seen=last_seen=datetime.now()
+        devices[body.device_id] = {
+    "first_seen": first_seen,
+    "last_seen": last_seen,
+    "mac_address": body.mac_address,
+    "first_meet": True}
+        
+    return Identification_response(
+        device_id=body.device_id,
+        first_seen=first_seen,
+        last_seen=last_seen,
+        first_meet=first_meet)
+       
+measure={}    
+@app.post("/measurements")
+def resp_measurements(device_id: str,body:Measurements_request):
+    if device_id not in measure:
+        measure[device_id] = []
+
+    measure[device_id].append({
+        "operator": body.operator,
+        "signal_power": body.signal_power,
+        "SNR ": body.SNR,
+        "network_type": body.network_type,
+        "frequency_band": body.frequency_band, 
+        "Cell_id": body.Cell_id ,
+        "time_stamp": datetime.now()})
+    
+    return Measuremnents_response(status="ok", 
+                                 measurement_num =len(measure[device_id]),
+                                                                   
+                                  server_time=datetime.now())
