be2d995699b75d2a19a98b792abe536f611a7429
GarodBederian <garodbedirian@gmail.com>
2026-03-20 00:30:02 +0200

    Add files via upload

---
+++ telehone_data/MeasurementRequest_function.kt
@@ -0,0 +1,76 @@
+package com.example.networkcellanalyzer.telephony
+import android.annotation.SuppressLint
+import android.content.Context
+import android.os.Build
+import android.telephony.TelephonyManager
+import android.telephony.CellInfoGsm
+import android.telephony.CellInfoWcdma
+import android.telephony.CellInfoLte
+import com.example.networkcellanalyzer.model.MeasurementRequest
+
+@SuppressLint("MissingPermission")
+fun readMeasurementFromPhone(context: Context, deviceId: String): MeasurementRequest? {
+
+    val tm = context.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+
+    var operator = tm.networkOperatorName
+    if (operator.isEmpty()) operator = "Unknown"
+
+    val cellList = tm.allCellInfo
+    if (cellList == null || cellList.isEmpty()) return null
+
+    val cell = cellList[0]
+
+
+    //4G
+    if (cell is CellInfoLte) {
+        val signal = cell.cellSignalStrength
+        var snr: Float? = null
+        var band: Int? = null
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {              //adnroid11 and above
+            snr = signal.rssnr.toFloat()
+            band = cell.cellIdentity.bands.firstOrNull()
+        }
+        return MeasurementRequest(
+            device_id = deviceId,
+            operator = operator,
+            signal_power = signal.dbm,
+            SNR = snr,
+            network_type = "4G",
+            frequency_band = band,
+            cell_id = cell.cellIdentity.ci.toString(),
+            time_stamp = System.currentTimeMillis().toString()
+        )
+    }
+    //3G
+    else if (cell is CellInfoWcdma) {
+        val signal = cell.cellSignalStrength
+        return MeasurementRequest(
+            device_id = deviceId,
+            operator = operator,
+            signal_power = signal.dbm,
+            SNR = null,
+            network_type = "3G",
+            frequency_band = null,
+            cell_id = cell.cellIdentity.cid.toString(),
+            time_stamp = System.currentTimeMillis().toString()
+        )
+    }
+    //2G
+    else if (cell is CellInfoGsm) {
+        val signal = cell.cellSignalStrength
+        return MeasurementRequest(
+            device_id = deviceId,
+            operator = operator,
+            signal_power = signal.dbm,
+            SNR = null,
+            network_type = "2G",
+            frequency_band = null,
+            cell_id = cell.cellIdentity.cid.toString(),
+            time_stamp = System.currentTimeMillis().toString()
+        )
+    }
+    else {
+        return null
+    }
+}
