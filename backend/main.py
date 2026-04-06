28ca0e964dae423c6f85fa2df79d2108e1597e6f
Hassan Takruri <hassantakruri@gmail.com>
2026-04-07 02:14:24 +0300

    Refactor measurements endpoint and stats calculation
    Updated the endpoint for measurements and modified the get_stats function to include device_id as a parameter. Enhanced statistics calculations and removed unnecessary filtering.

--- backend/main.py
+++ backend/main.py
@@ -52,7 +52,7 @@ def identify(body: Identification_request):
         first_meet=first_meet)
        
   
-@app.post("/measurements")
+@app.post("/measurement")
 def resp_measurements(device_id:str,body:Measurements_request):
     db=SessionLocal()
 
@@ -69,6 +69,9 @@ def resp_measurements(device_id:str,body:Measurements_request):
         )
         
     db.add(new_Measurment)
+    device = db.query(Device).filter(Device.device_id == body.device_id).first()
+    if device is not None:
+        device.last_seen = datetime.now(UTC)
     db.commit()
     num=db.query(Measurement).filter(Measurement.device_id == device_id).count()
     db.close()
@@ -82,31 +85,75 @@ def resp_measurements(device_id:str,body:Measurements_request):
 
 
 @app.get("/stats")
-def get_stats(from_date: datetime, to_date: datetime):
+def get_stats(device_id:str,from_date: datetime, to_date: datetime):
 
     db = SessionLocal()
     records = db.query(Measurement).filter(
+        Measurement.device_id == device_id,
         Measurement.time_stamp >= from_date,
         Measurement.time_stamp <= to_date
     ).all()
 
-    filtered = []
+    operator_label = {"touch":0, "alfa":1}
+    avg_connectivity_operator = [0,0]  # [TOUCH, ALFA]
+    network_label = {"2G":0,"3G":1,"4G":2,"5G":3}  
+    avg_connectivity_network = [0,0,0,0]  # [2G,3G,4G,5G]
+    avg_signal_power_networkType = [0,0,0,0]  # [2G,3G,4G,5G]
+    avg_signal_power_device = 0
+    avg_SNR_SNIR = [0,0,0,0]
+    snr_network_count = [0,0,0,0]
+    total_count = 0
+    total_count_snr = 0
 
-        for record in records:
-            if from_date <= record["time_stamp"] <= to_date:
-                filtered.append({
-                    "device_id": device_id,
-                    "operator": record["operator"],
-                    "network_type": record["network_type"],
-                    "signal_power": record["signal_power"],
-                    "SNR": record["SNR"],
-                    "time_stamp": record["time_stamp"]
-                })
-        db.close()
 
+    for record in records:
+       
+        total_count = total_count + 1
+        avg_signal_power_device += record.signal_power
+        if record.operator in operator_label:
+            tracker = operator_label[record.operator]
+            avg_connectivity_operator[tracker] += 1
+            
+        if record.network_type in network_label:
+            index = network_label[record.network_type]
+            avg_connectivity_network[index] +=1
+            avg_signal_power_networkType[index]+= record.signal_power
+            if record.SNR != None:
+                total_count_snr += 1
+                snr_network_count[index] +=1
+                avg_SNR_SNIR[index] += record.SNR
+                
+    
+    if total_count > 0:
+        avg_connectivity_operator[0] = avg_connectivity_operator[0]/total_count 
+        avg_connectivity_operator[1] = avg_connectivity_operator[1]/total_count 
+        avg_signal_power_device = avg_signal_power_device/total_count 
+        for i in range(0,4):
+            if avg_connectivity_network[i] != 0:
+                avg_signal_power_networkType[i] = avg_signal_power_networkType[i]/avg_connectivity_network[i]
+                
+            else:
+                avg_signal_power_networkType[i] = 0
+            avg_connectivity_network[i] = avg_connectivity_network[i]/total_count
+
+    else:
+        avg_connectivity_operator = [0, 0]
+        avg_connectivity_network = [0, 0, 0, 0]
+        avg_signal_power_networkType = [0, 0, 0, 0]
+        avg_signal_power_device = 0
+ 
+    for i in range(0,4):
+        if snr_network_count[i] != 0:
+            avg_SNR_SNIR[i] = avg_SNR_SNIR[i]/snr_network_count[i]        
+    
+ 
+    db.close()
     return {
         "from_date": from_date,
         "to_date": to_date,
-        "count": len(filtered),
-        "measurements": filtered
+        "avg_connectivity_operator": avg_connectivity_operator,
+        "avg_connectivity_network": avg_connectivity_network,
+        "avg_signal_power_networkType": avg_signal_power_networkType,
+        "avg_signal_power_device": avg_signal_power_device,
+        "avg_SNR_SNIR": avg_SNR_SNIR
     }
