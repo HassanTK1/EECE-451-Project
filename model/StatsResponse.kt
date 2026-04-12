d5e06d580bab1f07b608becfdbd9fbe1695e7990
Hassan Takruri <hassantakruri@gmail.com>
2026-04-12 15:21:45 +0300

    Refactor StatsResponse data types to use Double
    Updated StatsResponse data class to use Double types for connectivity and signal power fields.

--- model/StatsResponse.kt
+++ model/StatsResponse.kt
@@ -3,9 +3,9 @@ package com.example.networkcellanalyzer.model
 data class StatsResponse(
     val from_date: String,
     val to_date: String,
-    val avg_connectivity_operator: List<Map<String, Any>>,
-    val avg_connectivity_network: List<Map<String, Any>>,
-    val avg_signal_power_networkType: List<Map<String, Any>>,
-    val avg_signal_power_device: List<Map<String, Any>>,
-    val avg_SNR_SNIR: List<Map<String, Any>>
+    val avg_connectivity_operator: List<Double>,
+    val avg_connectivity_network: List<Double>,
+    val avg_signal_power_networkType: List<Double>,
+    val avg_signal_power_device: Double,
+    val avg_SNR_SNIR: List<Double>
 )
