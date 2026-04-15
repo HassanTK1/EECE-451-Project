e683d51aab45f151a50ceec009901b385dc14d6f
Hassan Takruri <hassantakruri@gmail.com>
2026-04-16 01:58:08 +0300

    Change DATABASE_URL to use environment variable
    Updated DATABASE_URL to use environment variable.

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine,Column,String,DateTime,Boolean,Integer,Floa
 from sqlalchemy.orm import sessionmaker,declarative_base
 import os
 
-DATABASE_URL = ("postgresql://root:KBNuZJLiMbf9foYDwXQ8fiDxwAkP3LlK@dpg-d7g1221o3t8c73c099u0-a/my_eece_database_on_render")
+DATABASE_URL = os.getenv("postgresql://root:KBNuZJLiMbf9foYDwXQ8fiDxwAkP3LlK@dpg-d7g1221o3t8c73c099u0-a/my_eece_database_on_render")
 engine = create_engine( DATABASE_URL, pool_pre_ping=True)
 
 SessionLocal = sessionmaker(bind=engine)
