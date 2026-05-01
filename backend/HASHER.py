c339a205438f768c024d53e3085c32986292cd33
Hassan Takruri <hassantakruri@gmail.com>
2026-05-01 22:01:54 +0300

    Add comments for password handling in HASHER.py
    Added comments to clarify password input and output usage.

--- backend/HASHER.py
+++ backend/HASHER.py
@@ -1,6 +1,7 @@
 import bcrypt
-
+# insert the password of your choosing in between quotations
 password = b"1234"
 hashed = bcrypt.hashpw(password, bcrypt.gensalt())
 
 print(hashed.decode())
+# paste the output in the env variable 
